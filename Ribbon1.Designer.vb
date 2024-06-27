d633f886e06ca79b0f1181e2d11cc126b5e80972
zgj9588 <2473649588@qq.com>
2024-06-27 12:07:29 +0800

    work1

---
+++ Ribbon1.Designer.vb
@@ -0,0 +1,96 @@
+﻿Partial Class Ribbon1
+    Inherits Microsoft.Office.Tools.Ribbon.RibbonBase
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New(ByVal container As System.ComponentModel.IContainer)
+        MyClass.New()
+
+        'Windows.Forms 类撰写设计器支持所必需的
+        If (container IsNot Nothing) Then
+            container.Add(Me)
+        End If
+
+    End Sub
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New()
+        MyBase.New(Globals.Factory.GetRibbonFactory())
+
+        '组件设计器需要此调用。
+        InitializeComponent()
+
+    End Sub
+
+    '组件重写释放以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '组件设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是组件设计器所必需的
+    '可使用组件设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.插件 = Me.Factory.CreateRibbonTab
+        Me.文档处理插件 = Me.Factory.CreateRibbonGroup
+        Me.启动纠错或总结 = Me.Factory.CreateRibbonButton
+        Me.插件.SuspendLayout()
+        Me.文档处理插件.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        '插件
+        '
+        Me.插件.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office
+        Me.插件.Groups.Add(Me.文档处理插件)
+        Me.插件.Label = "TabAddIns"
+        Me.插件.Name = "插件"
+        '
+        '文档处理插件
+        '
+        Me.文档处理插件.Items.Add(Me.启动纠错或总结)
+        Me.文档处理插件.Label = "文档处理插件"
+        Me.文档处理插件.Name = "文档处理插件"
+        '
+        '启动纠错或总结
+        '
+        Me.启动纠错或总结.Label = "启动纠错或总结"
+        Me.启动纠错或总结.Name = "启动纠错或总结"
+        Me.启动纠错或总结.ShowImage = True
+        '
+        'Ribbon1
+        '
+        Me.Name = "Ribbon1"
+        Me.RibbonType = "Microsoft.Word.Document"
+        Me.Tabs.Add(Me.插件)
+        Me.插件.ResumeLayout(False)
+        Me.插件.PerformLayout()
+        Me.文档处理插件.ResumeLayout(False)
+        Me.文档处理插件.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents 插件 As Microsoft.Office.Tools.Ribbon.RibbonTab
+    Friend WithEvents 文档处理插件 As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents 启动纠错或总结 As Microsoft.Office.Tools.Ribbon.RibbonButton
+End Class
+
+Partial Class ThisRibbonCollection
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Friend ReadOnly Property Ribbon1() As Ribbon1
+        Get
+            Return Me.GetRibbon(Of Ribbon1)()
+        End Get
+    End Property
+End Class
---
+++ Ribbon1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Diagnostics
+Imports System.IO
+Imports Microsoft.Office.Interop.Word
+Imports Microsoft.Office.Tools.Ribbon
+
+Public Class Ribbon1
+
+    Private Sub Ribbon1_Load(ByVal sender As System.Object, ByVal e As RibbonUIEventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As RibbonControlEventArgs) Handles 启动纠错或总结.Click
+        ' 获取当前插件的安装路径
+        Dim pluginPath As String = AppDomain.CurrentDomain.BaseDirectory
+        'MsgBox(pluginPath)
+        ' 设置可执行文件相对路径
+        Dim exeRelativePath As String = "main.exe"
+        Dim exeFullPath As String = Path.Combine(pluginPath, exeRelativePath)
+
+        ' 创建进程启动信息
+        Dim startInfo As New ProcessStartInfo()
+        startInfo.FileName = exeFullPath ' 设置可执行文件路径
+
+        Try
+            ' 启动可执行文件进程
+            Using process As Process = Process.Start(startInfo)
+                process.WaitForExit() ' 等待进程执行完毕
+                MsgBox("执行完毕！")
+            End Using
+        Catch ex As Exception
+            ' 处理启动进程时的异常
+            MsgBox("Error: " & ex.Message)
+        End Try
+    End Sub
+End Class
